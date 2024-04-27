3ed4fbb60b75ab3c9c7fae71c7437a7ad7b5df95
AndrewDrko <andrewmccartney09@gmail.com>
2024-04-26 21:34:53 -0600

    Carrito

--- js/script-servicios.js
+++ js/script-servicios.js
@@ -1,42 +1,42 @@
-const yearEl = document.querySelector('.year');
+const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-const headerEl = document.querySelector('.main-header');
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".main-header");
 
-btnNavEl.addEventListener('click', function () {
-  headerEl.classList.toggle('nav-open');
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
-const allLinks = document.querySelectorAll('a');
+const allLinks = document.querySelectorAll("a");
 
 allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    const href = link.getAttribute('href');
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
 
     // Check if the link is an internal anchor link
-    if (href && (href.startsWith('#') || href === '#')) {
+    if (href && (href.startsWith("#") || href === "#")) {
       e.preventDefault();
 
       // Scroll back to top
-      if (href === '#') {
+      if (href === "#") {
         window.scrollTo({
           top: 0,
-          behavior: 'smooth',
+          behavior: "smooth",
         });
       } else {
         // Scroll to other anchor links
         const sectionEl = document.querySelector(href);
         if (sectionEl) {
-          sectionEl.scrollIntoView({ behavior: 'smooth' });
+          sectionEl.scrollIntoView({ behavior: "smooth" });
         }
       }
 
       // Close mobile navigation
-      const headerEl = document.querySelector('header');
-      if (link.classList.contains('main-nav-link')) {
-        headerEl.classList.toggle('nav-open');
+      const headerEl = document.querySelector("header");
+      if (link.classList.contains("main-nav-link")) {
+        headerEl.classList.toggle("nav-open");
       }
     }
   });
@@ -47,74 +47,74 @@ allLinks.forEach(function (link) {
 /******************************************************************** */
 
 function cartMain() {
-  const productsContainer = document.querySelector('.products');
-  const numProductsElement = document.querySelector('.car-num');
+  const productsContainer = document.querySelector(".products");
+  const numProductsElement = document.querySelector(".car-num");
 
   if (!productsContainer && !numProductsElement) return;
 
   const products = [
     {
       id: 1,
-      name: 'Production musical',
-      imgUrl: '/img/Cartoon_music.jpg',
+      name: "Producción Musical",
+      imgUrl: "/img/Cartoon_music.jpg",
       description:
-        'Te ayudamos a proyectar tus ideas al siguiente nivel, realza la calidad musical de tus canciones y hazlas sonar profesionales.',
+        "Te ayudamos a proyectar tus ideas al siguiente nivel, realza la calidad musical de tus canciones y hazlas sonar profesionales.",
       specifications: [
-        'Puedes contar con la idea principal de tu canción o iniciar una desde cero.',
-        'Contar con una maqueta (Opcional)',
-        'Tener conocimientos básicos de teoría musical para poder abordar tus canciones.',
-        'El servicio cubre grabación, arreglos, composición y sound design.',
+        "Puedes contar con la idea principal de tu canción o iniciar una desde cero.",
+        "Contar con una maqueta (Opcional)",
+        "Tener conocimientos básicos de teoría musical para poder abordar tus canciones.",
+        "El servicio cubre grabación, arreglos, composición y sound design.",
       ],
       price: 399,
       quantity: 1,
     },
     {
       id: 2,
-      name: 'Mezcla',
-      imgUrl: '/img/Cartoon_music_production.jpg',
+      name: "Mezcla",
+      imgUrl: "/img/Cartoon_music_production.jpg",
       description:
-        'Envíanos tus tracks grabados y deja que nosotros le demos el sonido adecuado a tu canción bajo tus especificaciones.',
+        "Envíanos tus tracks grabados y deja que nosotros le demos el sonido adecuado a tu canción bajo tus especificaciones.",
       specifications: [
-        'Límite máximo de 40 pistas de stems.',
-        'La canción no debe exceder de 5 minutos (de lo contrario se aumenta el monto de servicio).',
-        'Es preferente que las pistas de stems estén previamente editadas (Opcional).',
-        'Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.',
+        "Límite máximo de 40 pistas de stems.",
+        "La canción no debe exceder de 5 minutos (de lo contrario se aumenta el monto de servicio).",
+        "Es preferente que las pistas de stems estén previamente editadas (Opcional).",
+        "Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.",
       ],
       price: 499,
       quantity: 1,
     },
     {
       id: 3,
-      name: 'Mastering',
-      imgUrl: '/img/Cartoon_masterin.jpg',
+      name: "Mastering",
+      imgUrl: "/img/Cartoon_masterin.jpg",
       description:
-        ' Envianos el bounce final de tu mezcla y nosotros nos encargamos de darle el volumen y calidad final para realzar las intenciones de la mezcla.',
+        " Envianos el bounce final de tu mezcla y nosotros nos encargamos de darle el volumen y calidad final para realzar las intenciones de la mezcla.",
       specifications: [
-        'Puedes enviar stems de instrumentos o un único track de mezcla.',
-        'La mezcla deberá tener un headroom máximo de -3.0db.',
-        'La mezcla debe cumplir con criterios mínimos para una correcta masterización.',
-        'Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.',
+        "Puedes enviar stems de instrumentos o un único track de mezcla.",
+        "La mezcla deberá tener un headroom máximo de -3.0db.",
+        "La mezcla debe cumplir con criterios mínimos para una correcta masterización.",
+        "Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.",
       ],
       price: 199,
       quantity: 1,
     },
     {
       id: 4,
-      name: 'Distribución a Plataformas de Streaming',
-      imgUrl: '/img/Cartoon_music_streaming.jpg',
+      name: "Distribución a Plataformas de Streaming",
+      imgUrl: "/img/Cartoon_music_streaming.jpg",
       description:
-        'Distribuye tu material musical en las plataformas de streaming más populares.',
+        "Distribuye tu material musical en las plataformas de streaming más populares.",
       specifications: [
-        'Deberás enviarnos tu track en un formato .wav o .mp3 de alta calidad (320kbps).',
-        'Deberás enviarnos: Nombre de Artista, Album (Single, EP o LP), Fecha de Publicación (Bajo disponibilidad), Caratula (3000px x 3000px), Personas Involucradas en la Obra (Productores, Compositores etc).',
-        'Deberás contar con alguna red social que identifique tu proyecto musical para registrarte en Spotify como Artista Verificado (En caso de no tener un perfil).',
+        "Deberás enviarnos tu track en un formato .wav o .mp3 de alta calidad (320kbps).",
+        "Deberás enviarnos: Nombre de Artista, Album (Single, EP o LP), Fecha de Publicación (Bajo disponibilidad), Caratula (3000px x 3000px), Personas Involucradas en la Obra (Productores, Compositores etc).",
+        "Deberás contar con alguna red social que identifique tu proyecto musical para registrarte en Spotify como Artista Verificado (En caso de no tener un perfil).",
       ],
       price: 99,
       quantity: 1,
     },
   ];
 
-  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
   const updateLocalStorage = (item, data) => {
     console.log(data);
@@ -129,13 +129,13 @@ function cartMain() {
     // Sum 1 to quantity if exist
     if (productCart) {
       productCart.quantity++;
-      updateLocalStorage('cart', cart);
+      updateLocalStorage("cart", cart);
       return;
     }
 
     const product = products.find((product) => product.id === id);
     cart.push({ ...product });
-    updateLocalStorage('cart', cart);
+    updateLocalStorage("cart", cart);
   };
 
   // Render products
@@ -156,7 +156,7 @@ function cartMain() {
                 .map((item) => {
                   return `<li>${item}</li>`;
                 })
-                .join(' ')}
+                .join(" ")}
             </ul>
           </div>
           <img
@@ -174,7 +174,7 @@ function cartMain() {
         </div>
       `;
 
-      productsContainer?.insertAdjacentHTML('beforeend', markupProduct);
+      productsContainer?.insertAdjacentHTML("beforeend", markupProduct);
     });
   };
 
@@ -184,9 +184,9 @@ function cartMain() {
   function handleAddProduct(e) {
     const btnProduct = e.target;
 
-    if (!btnProduct.classList.contains('button-car')) return;
+    if (!btnProduct.classList.contains("button-car")) return;
 
-    const productElement = e.target.closest('.square-production');
+    const productElement = e.target.closest(".square-production");
     const productId = +productElement.dataset.productId;
 
     addItemCart(productId);
@@ -194,14 +194,14 @@ function cartMain() {
   }
 
   // LISTEN EVENTS
-  productsContainer?.addEventListener('click', handleAddProduct);
+  productsContainer?.addEventListener("click", handleAddProduct);
 
   // INIT
   numProductsElement.innerHTML = getNumProducts();
 }
 
 export const getNumProducts = () => {
-  const cartRemote = localStorage.getItem('cart');
+  const cartRemote = localStorage.getItem("cart");
 
   if (!cartRemote) return 0;
 
