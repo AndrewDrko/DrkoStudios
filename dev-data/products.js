2d6e37eacdf6f81d823a47a66ede214f524b2d8d
abner-silva-dev <dylanabner14@gmail.com>
2024-04-26 23:15:42 -0600

    Fixing name element call, refactory carts funtionalitys

---
+++ dev-data/products.js
@@ -0,0 +1,61 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Producción Musical',
+    imgUrl: '/img/Cartoon_music.jpg',
+    description:
+      'Te ayudamos a proyectar tus ideas al siguiente nivel, realza la calidad musical de tus canciones y hazlas sonar profesionales.',
+    specifications: [
+      'Puedes contar con la idea principal de tu canción o iniciar una desde cero.',
+      'Contar con una maqueta (Opcional)',
+      'Tener conocimientos básicos de teoría musical para poder abordar tus canciones.',
+      'El servicio cubre grabación, arreglos, composición y sound design.',
+    ],
+    price: 399,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: 'Mezcla',
+    imgUrl: '/img/Cartoon_music_production.jpg',
+    description:
+      'Envíanos tus tracks grabados y deja que nosotros le demos el sonido adecuado a tu canción bajo tus especificaciones.',
+    specifications: [
+      'Límite máximo de 40 pistas de stems.',
+      'La canción no debe exceder de 5 minutos (de lo contrario se aumenta el monto de servicio).',
+      'Es preferente que las pistas de stems estén previamente editadas (Opcional).',
+      'Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.',
+    ],
+    price: 499,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    name: 'Mastering',
+    imgUrl: '/img/Cartoon_masterin.jpg',
+    description:
+      ' Envianos el bounce final de tu mezcla y nosotros nos encargamos de darle el volumen y calidad final para realzar las intenciones de la mezcla.',
+    specifications: [
+      'Puedes enviar stems de instrumentos o un único track de mezcla.',
+      'La mezcla deberá tener un headroom máximo de -3.0db.',
+      'La mezcla debe cumplir con criterios mínimos para una correcta masterización.',
+      'Las pistas deben estar grabadas en muestreo de 44.1 Khz y tasa de 24 bits.',
+    ],
+    price: 199,
+    quantity: 1,
+  },
+  {
+    id: 4,
+    name: 'Distribución a Plataformas de Streaming',
+    imgUrl: '/img/Cartoon_music_streaming.jpg',
+    description:
+      'Distribuye tu material musical en las plataformas de streaming más populares.',
+    specifications: [
+      'Deberás enviarnos tu track en un formato .wav o .mp3 de alta calidad (320kbps).',
+      'Deberás enviarnos: Nombre de Artista, Album (Single, EP o LP), Fecha de Publicación (Bajo disponibilidad), Caratula (3000px x 3000px), Personas Involucradas en la Obra (Productores, Compositores etc).',
+      'Deberás contar con alguna red social que identifique tu proyecto musical para registrarte en Spotify como Artista Verificado (En caso de no tener un perfil).',
+    ],
+    price: 99,
+    quantity: 1,
+  },
+];
